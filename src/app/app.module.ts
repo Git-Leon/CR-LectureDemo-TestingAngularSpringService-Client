cc33cd4b1779f88b7fd4780146e31a2eae420c2e
Leon <leon@Leons-MacBook-Pro.local>
2017-12-20 21:02:13 -0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,24 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { UserInputTableComponent } from './components/user-input-table/user-input-table.component';
+import { AddUserService } from './services/add-user.service';
+import { UserComponent } from './models/user/user.component';
+
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserInputTableComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [
+    AddUserService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/components/user-input-table/user-input-table.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-input-table',
+  templateUrl: './user-input-table.component.html',
+  styleUrls: ['./user-input-table.component.css']
+})
+export class UserInputTableComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
